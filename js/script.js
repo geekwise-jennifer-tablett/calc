6894be4eee2d37ac4ac601c594fb5196e38ebfb5
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-14 23:20:51 +0000

    tried tables with push button instead of input button

--- js/script.js
+++ js/script.js
@@ -1,108 +1,43 @@
-var create_input_tag;
-var read_input_tag;
-var update_input_tag;
-var delete_input_tag;
+function pushButton(buttonValue) {
+     if (buttonValue == 'C') {
+          document.getElementById('screen').value = '';
+     }
+     else {
+          document.getElementById('screen').value += buttonValue;
+     }
+}
+function calculate(equation) {
+     var answer = eval(equation);
+     document.getElementById('screen').value = answer;
+}
 
-var create_element = function(){
-    
-    var element = document.createElement();
-    element.style.display = 'block';
-    
-    return element;
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var get_input_id = function('id'){
+    return document.getElementById('id')
 };
 
-var attach_element = function(){
-    
-    document.body.appendChild(num_but);
-    
-};
-
-
-document.addEventListener('DOMContentLoaded',function(event){
-    
-    create_input_tag = create_element('input');
-    read_input_tag = create_element('input');
-    update_input_tag = create_element('input');
-    delete_input_tag = create_element('input');
-    
-    create_input_tag.placeholder = '+';
-    delete_input_tag.placeholder = '-';
-    update_input_tag.placeholder = '=';
-    
-    attach_element(create_input_tag);
-    attach_element(read_input_tag);
-    attach_element(update_input_tag);
-    attach_element(delete_input_tag);
-    
-    create_input_tag.addEventListener('click',function(event){
-        
-        if(){
-            
-            var new_element = document.createElement('value');
-            new_element.setAttribute('id', create_input_tag.value);
-            new_element.textContent = create_input_tag.value;
-            
-            attach_element(new_element);
-            
-        };
-        
-    });
-        
-    update_input_tag.addEventListener('click',function(event){
-        
-        if(){
-            
-            var element_to_update = document.getElementById(update_input_tag.value);
-            
-            
-        };
-        
-        
-    });
-    
-    
-});
-
-
-
-
-
-
-/*var element = document.num_but('num_but');
-var display
-var calc_body
-var num_but
-
-calc_body.num_but.addEventListener("click",function(){
-    this.value;
-     if(display.innerHTML === "") {
-            
-            display.innerHTML = display.innerHTML.concat("");
-            
-        }
-}); 
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded',function(event){
-   
-  var display = getElementById('result');
-    
-    console.log(event);
-});
-
-
-
-
-
-/*
+//adding
+document.getElementById(id+'_result').focus();
+        return true;
 var num_but = valueof('i');
 
+//subtraction
 
 //when number button clicked add to display
 calc_body.addEventListener('click',function(event){
@@ -111,6 +46,4 @@ calc_body.addEventListener('click',function(event){
     }
 });
    
-    
-
-*/
+    */
