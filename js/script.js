0166688893fa2e0a55a0591bf87dc4d26ba17562
geekwise_jennifer_tablett <geekwise.jennifer.tablett@gmail.com>
2016-04-14 19:17:44 +0000

    click function

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.calc_body.addEventListener('click',function(event){
+    
+    document.getElementById('one');
+    document.getElementById.innerHtml = '1';
+    text.content = display_number('i')   
+    
+});
+document.addEventListener('DOMContentLoaded',function(event){
+    display_number();
+    console.log(event);
+});
